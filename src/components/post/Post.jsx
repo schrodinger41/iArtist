35a9849517ec7cd9382813c719fde89d89404b34
jhet <jhetdizon41@gmail.com>
2024-08-21 22:30:15 +0800

    design draft homepage

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./post.css";
 
-const Post = ({ imageUrl, userName, userPhoto, caption }) => {
+const Post = ({ imageUrl, userName, userPhoto }) => {
   return (
     <div className="post">
       <div className="post-header">
         <img src={userPhoto} alt="User Profile" className="user-photo" />
         <p className="user-name">{userName}</p>
-      </div>
-      <div className="post-caption">
-        <p>{caption}</p>
       </div>
       <div className="post-image">
         <img src={imageUrl} alt="Uploaded content" />
