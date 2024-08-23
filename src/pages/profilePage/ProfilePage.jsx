efbad7ca9f7c1ca452d9c157bf3b7c837f846808
jhet <jhetdizon41@gmail.com>
2024-08-23 20:46:35 +0800

    test

--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../config/firebase";
 import Navbar from "../../components/navbar/Navbar";
-import "./profilePage.css";
 
 const ProfilePage = () => {
   const { userId } = useParams();
